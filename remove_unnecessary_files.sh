afaefc8512abe8c334be3459d60724eeffde91db
eg-itn <brokmn@gmail.com>
2023-02-19 01:03:04 +0900

    modify remove unnecessary files script

--- remove_unnecessary_files.sh
+++ remove_unnecessary_files.sh
@@ -1,5 +1,9 @@
+rm -rf users
+rm -rf layouts/community
 cd keyboards
 ls | grep -v -E 'preonic|readme.md' | xargs rm -rf
 cd preonic/keymaps
+rm -rf egstad
 ls | grep -v -E 'eg' | xargs rm -rf
 cd ../../..
+
